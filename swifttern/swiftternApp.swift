ca330d871b62f1e79aa81cec578a4b9a4a34e6e7
Ivan Galkin <ivangalkindeveloper@gmail.com>
2024-01-13 01:55:07 +0300

    Initial Commit

---
+++ swifttern/swiftternApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftternApp.swift
+//  swifttern
+//
+//  Created by Иван Галкин on 13.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct swiftternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
